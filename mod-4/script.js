b0355a23d42d64d0e34103a6818690363ecac401
srijitbhattacharjee <85347991+srijitbhattacharjee@users.noreply.github.com>
2021-06-18 16:29:55 +0530

    Update script.js

--- mod-4/script.js
+++ mod-4/script.js
@@ -1,1 +1,11 @@
-
+(function() {
+    var names = ["Srijit", "John", "Jenny","David", "Jason", "Paul", "Frank", "Paula", "Laura", "Jimmy"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
