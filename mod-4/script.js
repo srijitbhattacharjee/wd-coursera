e964670eac7251465fabde91f54dfb7ae3474ec5
srijitbhattacharjee <85347991+srijitbhattacharjee@users.noreply.github.com>
2021-06-18 16:36:26 +0530

    Update script.js

--- mod-4/script.js
+++ mod-4/script.js
@@ -1,5 +1,6 @@
+
 (function() {
-    var names = ["Srijit", "John", "Jenny","David", "Jason", "Paul", "Frank", "Paula", "Laura", "Jimmy"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank","Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
